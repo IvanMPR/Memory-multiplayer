8aa4675c9725a7b1b9649ff6ccc7210ff038772e
Ivan MPR <ivsanmas@gmail.com>
2023-05-23 22:02:39 +0200

    first commit

---
+++ public/utils/client-utils.js
@@ -0,0 +1,222 @@
+import { socket, helperObject, playersClient } from '../script.js';
+export const cards = [
+  'barcode',
+  'barcode',
+  'bitcoin',
+  'bitcoin',
+  'camera',
+  'camera',
+  'compass',
+  'compass',
+  'empty',
+  'empty',
+  'envelope',
+  'envelope',
+  'full',
+  'full',
+  'gauge',
+  'gauge',
+  'helicopter',
+  'helicopter',
+  'icons',
+  'icons',
+  'pattern',
+  'pattern',
+  'protractor',
+  'protractor',
+  'safe',
+  'safe',
+  'scope',
+  'scope',
+  'shutter',
+  'shutter',
+  'social',
+  'social',
+  'target',
+  'target',
+  'time',
+  'time',
+];
+
+export const cardFields = document.querySelectorAll('.gamefields');
+export const cardsContainer = document.querySelector('.cards-container');
+export const startButton = document.querySelector('.start-btn');
+export const restartButton = document.querySelector('.restart-btn');
+export const inputDiv = document.querySelector('.input-div');
+export const nameInput = document.querySelector('.name-input');
+export const nameButton = document.querySelector('.name-btn');
+export const infoDiv = document.querySelector('.info');
+export const playersNames = document.querySelectorAll('.player-h2');
+
+// ----------------------HELPER FUNCTIONS----------------------//
+//-------------------------------------------------------------//
+
+// Knuth-Yates shuffle function. ! Borrowed code ! Shuffles cards on start of the level
+export function shuffle(array) {
+  var currentIndex = array.length,
+    temporaryValue,
+    randomIndex;
+
+  //While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+    //Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    //And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+
+  return array;
+}
+
+export function resetHelperObject() {
+  helperObject.guesses = [];
+  helperObject.id = [];
+  cardsContainer.style.pointerEvents = 'initial';
+}
+
+export function toggleHideShow(el, classList = 'hidden') {
+  return document.getElementById(`${el}`).classList.toggle(`${classList}`);
+}
+
+export function addHiddenClass(el, classList = 'hidden') {
+  return document.getElementById(`${el}`).classList.add(`${classList}`);
+}
+
+export function displayUiMessage(message) {
+  infoDiv.innerHTML = '';
+  infoDiv.textContent = message;
+}
+
+export function renderPlayerNames(playersArray) {
+  playersArray.forEach((player, i) => {
+    playersNames[i].textContent = `${player.username}`;
+  });
+}
+
+export function markActivePlayer(activePlayer) {
+  playersNames.forEach(name => {
+    name.classList.remove('active-player');
+  });
+  document
+    .querySelector(`.player-${activePlayer}-h2`)
+    .classList.add('active-player');
+}
+
+export function clearActivePlayer() {
+  playersNames.forEach(name => {
+    name.classList.remove('active-player');
+  });
+}
+
+export function displayCurrentPlayerTurn() {
+  const activePlayerName = document.querySelector(
+    `.player-${helperObject.activePlayer}-h2`
+  ).textContent;
+  displayUiMessage(`${activePlayerName} is on the move!`);
+}
+
+export function preventWrongSocketInput() {
+  const name = playersClient.filter(player => player.id === socket.id)[0]
+    .username;
+  const activePlayerName = document.querySelector('.active-player').textContent;
+
+  return name !== activePlayerName;
+}
+
+export function flashWrongMoveWarning(oldMessage) {
+  const activePlayerName = document.querySelector('.active-player').textContent;
+  displayUiMessage(`It's not your turn, ${activePlayerName} is on the move!`);
+  errorTone();
+  setTimeout(() => {
+    displayUiMessage(oldMessage);
+  }, 1500);
+}
+
+export function flipActivePlayer() {
+  helperObject.activePlayer === 0 ? 1 : 0;
+  // const newActivePlayer = currentActivePlayer === 0 ? 1 : 0;
+  markActivePlayer(helperObject.activePlayer);
+  displayCurrentPlayerTurn();
+}
+
+export function showRestartButton() {
+  restartButton.closest('.restart-button-div').classList.remove('inactive');
+}
+
+export function winner(position) {
+  const winner = document.querySelector(`.player-${position}-h2`).textContent;
+  displayUiMessage(`${winner} won the game!`);
+  endGameTone();
+  showRestartButton();
+  clearActivePlayer();
+  cardsContainer.style.pointerEvents = 'none';
+}
+
+export function declareWinner(score) {
+  if (score[0] > score[1]) {
+    winner(0);
+  } else if (score[0] < score[1]) {
+    winner(1);
+  } else {
+    displayUiMessage(`It's a tie!`);
+    endGameTone();
+    showRestartButton();
+  }
+}
+
+export function restartGame() {
+  this.closest('.restart-button-div').classList.add('inactive');
+  resetHelperObject();
+  socket.emit('restart game');
+}
+
+export function startGame(shuffledCardsArray) {
+  startGameTone();
+  cardFields.forEach((field, i) => {
+    field.style.backgroundImage = `url(./memory_cards/${shuffledCardsArray[i]}.jpg)`;
+    field.classList.add('hidden');
+  });
+  document
+    .querySelectorAll('.gf-wrapper')
+    .forEach(wrapper => wrapper.classList.remove('hidden'));
+  cardsContainer.style.pointerEvents = 'initial';
+}
+
+// ---------------------------------------------- Audio files ----------------------------------------------//
+// --------------------------------------------------------------------------------------------------------//
+
+export const flipSound = function () {
+  const tone = new Audio('./sounds/369960__mischy__umblattern-kurz.wav');
+  return tone.play();
+};
+
+export const pairHit = function () {
+  const tone = new Audio('./sounds/528867__eponn__beep-5.wav');
+  return tone.play();
+};
+
+export const errorTone = function () {
+  const tone = new Audio('./sounds/344687__korground__error-sound.wav');
+  return tone.play();
+};
+
+export const pairMissTone = function () {
+  const tone = new Audio(
+    './sounds/140773__qubodup__computer-beep-sfx-for-videogames.wav'
+  );
+  return tone.play();
+};
+
+export const startGameTone = function () {
+  const tone = new Audio('./sounds/350872__cabled-mess__coin-c-01.wav');
+  return tone.play();
+};
+
+export const endGameTone = function () {
+  const tone = new Audio('./sounds/320775__rhodesmas__win-02.wav');
+  return tone.play();
+};
---
+++ server.js
@@ -0,0 +1,127 @@
+const http = require('http');
+const express = require('express');
+const socketio = require('socket.io');
+const path = require('path');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
+
+const {
+  cards,
+  shuffle,
+  updateScore,
+  resetScore,
+  isGameOver,
+  playersServer,
+  score,
+} = require('./public/utils/utils');
+
+// Set static folder
+app.use(express.static(path.join(__dirname, 'public')));
+// --------------- SOCKETS HANDLING -------------------- //
+io.on('connection', socket => {
+  socket.on('new player', username => {
+    // create player object
+    const player = {
+      username,
+      id: socket.id,
+      score: 0,
+    };
+
+    // if two playersServer are already connected, reject attempted connection
+    if (playersServer.length === 2) {
+      socket.emit('no room');
+      console.log('disconnected');
+      socket.disconnect();
+      return;
+    }
+    // add newly connected player to playersServer array
+    playersServer.push(player);
+
+    if (playersServer.length === 1) {
+      socket.emit('waiting for second player');
+    }
+    if (playersServer.length === 2) {
+      io.emit('both players connected', { memoryCards: shuffle(cards) });
+      resetScore(score);
+      // start game after 1.5s
+      setTimeout(() => {
+        let activePlayer = Math.floor(Math.random() * 2);
+        io.emit('active player', activePlayer);
+      }, 1500);
+    }
+
+    io.emit('new player connected', playersServer);
+  });
+
+  socket.on('card opened', openedCardId => {
+    socket.broadcast.emit('show opened card', openedCardId);
+  });
+
+  socket.on('same card clicked', data => {
+    socket.broadcast.emit('same card invalid move', data);
+    setTimeout(() => {
+      const newActivePlayer = data.activePlayer === 0 ? 1 : 0;
+      io.emit('active player change', newActivePlayer);
+    }, 1500);
+  });
+
+  socket.on('true pair', data => {
+    socket.broadcast.emit('close true pair', data);
+    updateScore(score, data.activePlayer);
+    io.emit('update score', score);
+    if (isGameOver(score, cards.length / 2)) {
+      io.emit('game over', score);
+    }
+  });
+
+  socket.on('missed pair', data => {
+    socket.broadcast.emit('close missed pair', data);
+    setTimeout(() => {
+      const newActivePlayer = data.activePlayer === 0 ? 1 : 0;
+      io.emit('active player change', newActivePlayer);
+    }, 1500);
+  });
+
+  socket.on('restart game', () => {
+    io.emit('restart game board');
+    io.emit('both players connected', { memoryCards: shuffle(cards) });
+    resetScore(score);
+    // start game after 1.5s
+    setTimeout(() => {
+      let activePlayer = Math.floor(Math.random() * 2);
+      io.emit('active player', activePlayer);
+      io.emit('update score', score);
+    }, 1500);
+    io.emit('new player connected', playersServer);
+  });
+
+  // ------------------- IF PLAYER LEAVES DURING THE GAME --------------------- //
+  socket.on('disconnect', () => {
+    // disconnected player id
+    const id = socket.id;
+    // declare variables that will be sent by the 'player left event'
+    let position;
+    let playerName;
+
+    if (!playersServer.some(player => player.id === id)) {
+      return;
+    }
+    // delete disconnected player from players array
+    if (playersServer.length > 0) {
+      // determine if first or second player has left the game
+      position = playersServer[0].id === id ? 0 : 1;
+      // get username of the player who left
+      playerName = playersServer[position].username;
+      // remove first or last player from the array, depending who has left
+      position === 0 ? playersServer.shift() : playersServer.pop();
+    }
+
+    // emit left event, and send data about player who has left, that will be used to inform remaining player
+    io.emit('player left game', { playerName, position });
+  });
+});
+// --------------- PORT ---------------- //
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
